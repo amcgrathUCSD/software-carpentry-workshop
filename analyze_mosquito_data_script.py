4ffa938d42413938d7d4ae7f42069cf92a14afcd
Aaron McGrath <amcgrath@ucsd.edu>
2014-10-21 14:08:27 -0700

    analyse saves an output figure

--- analyze_mosquito_data_script.py
+++ analyze_mosquito_data_script.py
@@ -6,7 +6,7 @@ filename = "A1_mosquito_data.csv"
 data  = pd.read_csv(filename)
 data["temperature"] = mosquito_lib.fahr_to_celsius(data["temperature"])
 
-parameters = mosquito_lib.analyze(data)
+parameters = mosquito_lib.analyze(data, "plot.png")
 
 parameters.to_csv("parameters.csv")
 

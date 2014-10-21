1d52249788df62d1a33f98b716a321aad4eca324
Aaron McGrath <amcgrath@ucsd.edu>
2014-10-21 13:48:24 -0700

    edited the analyze_mosquito_data_script.py

--- analyze_mosquito_data_script.py
+++ analyze_mosquito_data_script.py
@@ -4,8 +4,12 @@ import analyze_mosquito_data_lib as mosquito_lib
 filename = "A1_mosquito_data.csv"
 
 data  = pd.read_csv(filename)
+data["temperature"] = mosquito_lib.fahr_to_celsius(data["temperature"])
 
-print data.head()
+parameters = mosquito_lib.analyze(data)
 
-print "Hello!"
+parameters.to_csv("parameters.csv")
 
+# save parameters to file
+
+print parameters
